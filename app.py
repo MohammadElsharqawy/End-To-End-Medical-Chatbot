c54392d4fc4da49017763afcfc22a92c3504bf36
mohamedElsharqawy <melsharkawy99@hotmail.com>
2025-08-19 17:26:38 +0300

    Add Docker file

--- app.py
+++ app.py
@@ -58,8 +58,7 @@ def index():
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    print(input)
+    print(msg)
     response = rag_chain.invoke({"input": msg})
     print("Response : ", response["answer"])
     return str(response["answer"])
